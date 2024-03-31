bb7f11d490c07560cc384cbdbd17e5b2ac08c6bd
vinod.mby@gmail.com <vinod madubashana>
2024-03-31 21:41:24 +0530

    init from - https://github.com/tmatyashovsky/spark-ml-samples

---
+++ api/src/main/java/com/lohika/morning/ml/api/service/DouService.java
@@ -0,0 +1,57 @@
+package com.lohika.morning.ml.api.service;
+
+import com.lohika.morning.ml.spark.driver.service.dou.DouMLService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DouService {
+
+    @Autowired
+    private DouMLService douMLService;
+
+//    @Value("${dou.training.set.csv.file.path}")
+    private String douTrainingSetCsvFilePath;
+
+//    @Value("${dou.regression.model.directory.path}")
+    private String douRegressionModelDirectoryPath;
+
+//    @Value("${dou.clustering.model.directory.path}")
+    private String douClusteringModelDirectoryPath;
+
+    public Map<String, Object> trainDOURegressionModel() {
+        return douMLService.trainDOURegressionModel(douTrainingSetCsvFilePath, douRegressionModelDirectoryPath);
+    }
+
+    public Double predictSalary(Double experience, String englishLevel, String programmingLanguage) {
+        return douMLService.predictSalary(experience, englishLevel, programmingLanguage, douRegressionModelDirectoryPath);
+    }
+
+    public Map<String, Object> clusterizeITSpecialists(Integer clusters) {
+        return douMLService.clusterizeITSpecialists(douTrainingSetCsvFilePath, clusters, douClusteringModelDirectoryPath);
+    }
+
+    public Level predictLevel(Integer salary, Double experience, String englishLevel) {
+        Integer prediction = douMLService.predictLevel(salary, experience, englishLevel, douClusteringModelDirectoryPath);
+
+        switch (prediction) {
+            case 0: {
+                return Level.JUNIOR;
+            }
+
+            case 1: {
+                return Level.SENIOR;
+            }
+
+            case 2: {
+                return Level.MIDDLE;
+            }
+
+            default: {
+                return Level.UNKNOWN;
+            }
+        }
+    }
+}
---
+++ api/src/main/java/com/lohika/morning/ml/api/service/MnistService.java
@@ -0,0 +1,80 @@
+package com.lohika.morning.ml.api.service;
+
+import com.lohika.morning.ml.spark.driver.service.MLlibService;
+import com.lohika.morning.ml.spark.driver.service.mnist.MnistUtilityService;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.spark.mllib.classification.LogisticRegressionModel;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MnistService {
+
+//    @Value("${mnist.training.set.image.file.path}")
+    private String mnistTrainingSetImageFilePath;
+
+//    @Value("${mnist.training.set.label.file.path}")
+    private String mnistTrainingSetLabelFilePath;
+
+//    @Value("${mnist.test.set.image.file.path}")
+    private String mnistTestSetImageFilePath;
+
+//    @Value("${mnist.test.set.label.file.path}")
+    private String mnistTestSetLabelFilePath;
+
+//    @Value("${mnist.training.set.parquet.file.path}")
+    private String mnistTrainingSetParquetFilePath;
+
+//    @Value("${mnist.test.set.parquet.file.path}")
+    private String mnistTestSetParquetFilePath;
+
+//    @Value("${mnist.model.directory.path}")
+    private String mnistModelDirectoryPath;
+
+//    @Value("${mnist.validation.set.directory.path}")
+    private String mnistValidationSetDirectoryPath;
+
+    @Autowired
+    private MLlibService mLlibService;
+
+    @Autowired
+    private MnistUtilityService mnistUtilityService;
+
+    public Map<String, Object> trainImages() {
+        return mLlibService.trainLogisticRegression(mnistTrainingSetParquetFilePath,
+                                                    mnistTestSetParquetFilePath,
+                                                    10,
+                                                    mnistModelDirectoryPath);
+    }
+
+    public Map<String, Object> predict() {
+        LogisticRegressionModel model = mLlibService.loadLogisticRegression(mnistModelDirectoryPath);
+
+        LabeledPoint labeledPoint = mnistUtilityService.loadRandomImage(mnistTestSetImageFilePath,
+                                                                        mnistTestSetLabelFilePath,
+                                                                        mnistModelDirectoryPath);
+
+        Double prediction = model.predict(labeledPoint.features());
+
+        Map<String, Object> predictions = new HashMap<>();
+        predictions.put("Prediction", prediction);
+        predictions.put("Actual", labeledPoint.label());
+
+        System.out.println("\n------------------------------------------------");
+        System.out.println(predictions);
+        System.out.println("------------------------------------------------\n");
+
+        return predictions;
+    }
+
+    public void convertMnist() {
+        mnistUtilityService.convertMnistDatasetToParquet(
+            mnistTrainingSetImageFilePath, mnistTrainingSetLabelFilePath, 60000, mnistTrainingSetParquetFilePath);
+
+        mnistUtilityService.convertMnistDatasetToParquet(
+            mnistTestSetImageFilePath, mnistTestSetLabelFilePath, 10000, mnistTestSetParquetFilePath);
+    }
+}
